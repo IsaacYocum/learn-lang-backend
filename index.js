e0984b23281cd5c5a59b25f64249f42d53c501f2
isaac <isyocum@gmail.com>
2022-01-06 22:40:23 -0700

    added text file location

--- index.js
+++ index.js
@@ -6,7 +6,25 @@ app.use(express.static('texts'))
 app.use(cors())
 
 app.get('/api/texts', (req, res) => {
-    res.sendFile('sample.txt', {root: './texts'})
+  const testFolder = './texts/';
+  const fs = require('fs');
+
+  fs.readdir(testFolder, (err, files) => {
+    if (err) throw err;
+    let texts = {
+      texts: []
+    }
+    files.forEach(file => {
+      texts['texts'].push(file)
+    });
+    res.send(texts)
+  });
+  // res.sendFile('sample.txt', {root: './texts'})
+})
+
+app.get('/api/texts/:text', (req, res) => {
+  let text = req.params.text;
+  res.sendFile(text, {root: './texts'})
 })
 
 const PORT = 3001
