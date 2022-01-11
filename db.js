5cf8b46763f6b655540cf0dd7cbbcd40e881a537
Isaac Yocum <isyocum@gmail.com>
2022-01-10 19:31:56 -0700

    Added sqlite3 database

--- db.js
+++ db.js
@@ -1,11 +1,39 @@
 const sqlite3 = require('sqlite3').verbose();
 
 // open database in memory
-let db = new sqlite3.Database(':memory:', (err) => {
+let db = new sqlite3.Database('./db/learn-lang.db', (err) => {
   if (err) {
     return console.error(err.message);
   }
-  console.log('Connected to the in-memory SQlite database.');
+  console.log('Connected to the SQlite database.');
+});
+
+db.get("PRAGMA foreign_keys = ON")
+
+// setup tables
+db.serialize(() => {
+  db.run(`CREATE TABLE languages(language TEXT PRIMARY KEY)`)
+    .run(`INSERT INTO languages(language)
+          VALUES ('english'),
+                 ('vietnamese')`)
+    .each(`SELECT language from languages`, (err, row) => {
+      if (err) throw err;
+      console.log(row.language)
+    })
+
+  db.run(`CREATE TABLE words(wordId INTEGER PRIMARY KEY, 
+                             language TEXT,
+                             word TEXT,
+                             FOREIGN KEY(language) REFERENCES languages(language))`)
+    .run(`INSERT INTO words(language, word)
+          VALUES ('english', 'the'),
+                 ('english', 'boy'),
+                 ('english', 'who'),
+                 ('english', 'lived')`)
+    .each(`SELECT word from words`, (err, row) => {
+      if (err) throw err;
+      console.log(row.word)
+    })
 });
 
 // close the database connection
--- index.js
+++ index.js
@@ -1,33 +1,83 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./db/learn-lang.db')
 
 app.use(express.static('texts'))
 app.use(cors())
 
 app.get('/api/texts', (req, res) => {
-  const testFolder = './texts/';
-  const fs = require('fs');
+    const testFolder = './texts/';
+    const fs = require('fs');
 
-  fs.readdir(testFolder, (err, files) => {
-    if (err) throw err;
-    let texts = {
-      texts: []
-    }
-    files.forEach(file => {
-      texts['texts'].push(file)
+    fs.readdir(testFolder, (err, files) => {
+        if (err) throw err;
+        let texts = {
+            texts: []
+        }
+        files.forEach(file => {
+            texts['texts'].push(file)
+        });
+        res.send(texts)
     });
-    res.send(texts)
-  });
-  // res.sendFile('sample.txt', {root: './texts'})
+    // res.sendFile('sample.txt', {root: './texts'})
 })
 
 app.get('/api/texts/:text', (req, res) => {
-  let text = req.params.text;
-  res.sendFile(text, {root: './texts'})
+    let text = req.params.text;
+    res.sendFile(text, { root: './texts' })
+})
+
+// Languages endpoints
+app.get('/api/languages', (req, res) => {
+    db.all('SELECT * FROM languages', (err, rows) => {
+        if (err) throw err;
+        console.log(rows);
+        res.json(rows)
+    })
+})
+
+// Languages endpoints
+app.get('/api/languages/:language', (req, res) => {
+    let language = req.params.language;
+    console.log(language)
+    db.all(`SELECT * FROM languages
+            WHERE language = '${language}'`, (err, rows) => {
+        if (err) {
+            res.status(404).send(`404: The language '${language}' does not exist.`)
+            throw err;
+        }
+    
+        res.json(rows)
+    })
+})
+
+// Languages endpoints
+app.get('/api/languages/:language/words', (req, res) => {
+    db.all('SELECT * FROM words', (err, rows) => {
+        if (err) throw err;
+        console.log(rows);
+        res.json(rows)
+    })
+})
+
+// Languages endpoints
+app.get('/api/languages/:language/words', (req, res) => {
+    let language = req.params.language;
+    console.log(language)
+    db.all(`SELECT * FROM words
+            WHERE word = '${language}'`, (err, rows) => {
+        if (err) {
+            res.status(404).send(`404: The language '${language}' does not exist.`)
+            throw err;
+        }
+    
+        res.json(rows)
+    })
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
