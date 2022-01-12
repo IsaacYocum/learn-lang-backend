db4f43502540c5d6602d1089c5e506121848aa63
isaac <isyocum@gmail.com>
2022-01-11 20:42:15 -0700

    fixing port issue

--- index.js
+++ index.js
@@ -88,6 +88,6 @@ app.get('/api/languages/:language/words/:word', (req, res) => {
 })
 
 const PORT = 3001
-app.listen(PORT, () => {
+app.listen(process.env.port || PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
